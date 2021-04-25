773ee82c3e224d90677a3ff6227c435a1911b135
bpetryshyn <b.y.petryshyn@gmail.com>
2021-04-25 14:29:25 +0300

    Make http client call local service while in debug mode

--- common/api/createAppClient.ts
+++ common/api/createAppClient.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 export default function createAppClient() {
+  const baseURL = __DEV__
+    ? 'http://192.168.0.228:3000/nails'
+    : 'http://load-loadb-J7NVSRJA7C8E-1060774176.eu-central-1.elb.amazonaws.com/nails';
   return axios.create({
-    baseURL:
-      'http://load-loadb-J7NVSRJA7C8E-1060774176.eu-central-1.elb.amazonaws.com/nails',
+    baseURL,
   });
 }

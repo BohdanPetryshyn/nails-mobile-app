7d56e0e23027fa232709fa6b0a69604167783391
bpetryshyn <b.y.petryshyn@gmail.com>
2021-05-04 10:06:27 +0300

    Style SelectRole screen

---
+++ auth/components/RoleButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { Button } from '@ui-kitten/components';
+import selectRole from '../store/actions/selectRole';
+import { Role } from '../entities/User';
+
+export function RoleButton({ role, text }: { role: Role; text: string }) {
+  const dispatch = useDispatch();
+
+  return (
+    <Button
+      size="giant"
+      appearance="outline"
+      onPress={() => dispatch(selectRole(role))}
+      style={styles.button}
+    >
+      {text}
+    </Button>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: { margin: 10, width: '50%' },
+});
--- auth/screens/SelectRole.tsx
+++ auth/screens/SelectRole.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
-import { Text, View } from '../../components/Themed';
+import { StyleSheet } from 'react-native';
+import { Button, Text } from '@ui-kitten/components';
 import { useDispatch } from 'react-redux';
 import selectRole from '../store/actions/selectRole';
 import { Role } from '../entities/User';
+import { SafeAreaLayout } from '../../common/components/SafeAreaLayout';
+import { RoleButton } from '../components/RoleButton';
 
 export default function () {
   const dispatch = useDispatch();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Select Role</Text>
-      <Button
-        title="Client"
-        onPress={() => dispatch(selectRole(Role.CLIENT))}
-      />
-      <Button
-        title="Master"
-        onPress={() => dispatch(selectRole(Role.MASTER))}
-      />
-    </View>
+    <SafeAreaLayout style={styles.container}>
+      <Text category="h5" status="primary" style={styles.text}>
+        Хто Ви?
+      </Text>
+      <RoleButton role={Role.CLIENT} text="Клієнт" />
+      <RoleButton role={Role.MASTER} text="Майстер" />
+    </SafeAreaLayout>
   );
 }
 
@@ -26,15 +24,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: 100,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  text: { marginBottom: 15 },
 });

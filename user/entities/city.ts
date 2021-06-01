f8362bafe9a7e0ff057ffe579574cef4300ba2b6
bpetryshyn <b.y.petryshyn@gmail.com>
2021-06-01 12:46:22 +0300

    Add new cities and service types

--- user/entities/city.ts
+++ user/entities/city.ts
@@ -1,11 +1,19 @@
 export enum City {
   LVIV = 'LVIV',
   KYIV = 'KYIV',
-  DUBROVYTSIA = 'DUBROVYTSIA',
+  ODESSA = 'ODESSA',
+  KHARKIV = 'KHARKIV',
+  DNIPRO = 'DNIPRO',
+  TERNOPIL = 'TERNOPIL',
+  RIVNE = 'RIVNE',
 }
 
 export const CITY_LABELS = {
   [City.KYIV]: 'Київ',
   [City.LVIV]: 'Львів',
-  [City.DUBROVYTSIA]: 'Дубровиця',
+  [City.ODESSA]: 'Одеса',
+  [City.KHARKIV]: 'Харків',
+  [City.DNIPRO]: 'Дніпро',
+  [City.TERNOPIL]: 'Тернопіль',
+  [City.RIVNE]: 'Рівне',
 };
--- user/entities/service-type.ts
+++ user/entities/service-type.ts
@@ -2,10 +2,20 @@ export enum ServiceType {
   REMOVAL = 'REMOVAL',
   CLEANING = 'CLEANING',
   COATING = 'COATING',
+  DESIGN = 'DESIGN',
+  BUILD_UP = 'BUILD_UP',
+  REINFORCEMENT = 'REINFORCEMENT',
+  FRENCH = 'FRENCH',
+  PEDICURE = 'PEDICURE',
 }
 
 export const SERVICE_TYPE_LABELS = {
   [ServiceType.REMOVAL]: 'Зняття',
   [ServiceType.CLEANING]: 'Чистка',
   [ServiceType.COATING]: 'Покриття',
+  [ServiceType.DESIGN]: 'Дизайн',
+  [ServiceType.BUILD_UP]: 'Нарощення',
+  [ServiceType.REINFORCEMENT]: 'Укріплення',
+  [ServiceType.FRENCH]: 'Французький манікюр',
+  [ServiceType.PEDICURE]: 'Педикюр',
 };
